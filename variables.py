4a48b77e5213ee180bb77ef432a3b04157c4964c
Alejandro Lujan <alexlujanm@outlook.com>
2021-03-26 16:22:36 +0000

    clase variables 26 diciembre

---
+++ variables.py
@@ -0,0 +1,16 @@
+# variables.py
+# Author: Alejandro Lujan Merino
+# Oct, 3, 2021
+
+year = 2020
+sentence = "My name is alejandro"
+sarah, bob, mike = 16,20,25
+name, age = "mary", 24
+PI = 2.1416
+e = 2.71
+
+print("Year = ", year)
+print(sentence)
+print PI
+print e
+
