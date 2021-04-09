40d1df9785ee5d9b205f038c2e724fea696bbdf9
Alejandro Lujan <alexlujanm@outlook.com>
2021-04-09 14:47:21 +0000

    Lists

---
+++ lists.py
@@ -0,0 +1,38 @@
+# lists
+
+adnseq = ["A", "C", "T", "G", "A", "T", "G", "T", "A", "C"]
+adnseq1 = ["T", "G", "A", "T", "G"]
+
+n = len(adnseq)
+print adnseq
+print "length = ", n
+print "first position = ", adnseq[0]
+print "last position = ", adnseq[n-1]
+print "first 3 positions = ", adnseq[0:3]
+print "last 5 positions = ", adnseq[-5:]
+
+
+print "inserting one item..."
+adnseq.append("T")
+n = len(adnseq)
+print adnseq
+print "length = ", n
+
+print "replace firt position with T"
+adnseq[0] = "T"
+print len(adnseq)
+print adnseq
+
+print "delete last position"
+adnseq.pop(n-1)
+print adnseq
+
+print "new adnseq = seq1 + seq2"
+newadnseq = adnseq + adnseq1
+
+print adnseq
+print adnseq1
+print newadnseq
+print "length = ", len(newadnseq)
+
+
